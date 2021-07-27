88d3f9f6826200ed4f486341b73df7eec1683dfe
wajayanath <wajayanath@gmail.com>
2021-07-27 16:00:33 +0530

    Login user with logout

--- vue-admin/src/components/Nav.vue
+++ vue-admin/src/components/Nav.vue
@@ -1,20 +1,33 @@
 <template>
     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
         <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Company name</a>
-        <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-        <ul class="navbar-nav px-3">
-            <li class="nav-item text-nowrap">
-                <a class="nav-link" href="#">Sign out</a>
-            </li>
-        </ul>
+
+        <nav class="my-2 my-md-0 mr-md-3">
+            <router-link to="/profile" class="p-2 text-white">{{user?.first_name}} {{user?.last_name}}</router-link>
+                <a class="p-2 text-white" href="javascript:void(0)" @click="logout">Sign out</a>
+        </nav>
     </nav>
 </template>
 
 <script>
+    import {useRouter} from 'vue-router';
+
     export default {
-        name: "Nav"
+        name: "Nav",
+        props:['user'],
+
+        setup() {
+            const router = useRouter();
+
+            const logout = () => {
+                localStorage.clear();
+                router.push('/login');
+            }
+
+            return {
+                logout
+            }
+
+        }
     }
 </script>
